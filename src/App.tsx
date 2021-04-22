b322aa00d57d820297b9c515d324a45826ee6a89
yuanlijian <yuanlijianvip@163.com>
2021-04-22 19:15:32 +0800

    创建class类组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/logo.svg';
 import robots from './mockdata/robots.json';
 import Robot from "./components/Robot";
 import styles from './App.module.css';
@@ -7,6 +7,10 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo"/>
+        <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
+      </div>
       <div className={styles.robotList}>
         {robots.map( r => <Robot id={r.id} email={r.email} name={r.name}/>)}
       </div>
